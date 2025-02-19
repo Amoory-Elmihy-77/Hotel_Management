0628c35120170c3297420aae330af26ab3029e30
3moory <amaryasser2358@gmail.com>
2025-02-19 16:53:11 +0200

    create class Employee

---
+++ Hotel_project/Hotel_Classes/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Hotel_Classes
+{
+    // Interface
+    interface IEmployeeActions
+    {
+        double CalculateSalary();
+        void Work();
+    }
+    internal abstract class Employee : Person, IEmployeeActions
+    {
+        protected double Salary;
+        public Employee(int id, string name, string phone, double salary) :base(id, name, phone)
+        {
+            this.Salary = salary;
+        }
+        public abstract double CalculateSalary();
+        public abstract void Work();
+    }
+}
