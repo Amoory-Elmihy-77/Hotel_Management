3ea5450b37e05daf99b01a617eabf940ca2fe18b
3moory <amaryasser2358@gmail.com>
2025-02-19 17:09:09 +0200

    create class Manager

---
+++ Hotel_project/Hotel_Classes/Manager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Hotel_Classes
+{
+    internal sealed class Manager : Employee
+    {
+        public Manager(int id, string name, string phone, double salary) : base(id, name, phone, salary) { }
+
+        public override double CalculateSalary()
+        {
+            return Salary * 1.2;
+        }
+
+        public override void Work()
+        {
+            Console.WriteLine("Manager is overseeing hotel operations.");
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"I am {Name}, and I am The Manager");
+        }
+    }
+}
