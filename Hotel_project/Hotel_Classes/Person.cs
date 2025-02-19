28931397a3d984750e45031288eb08830078bfc0
3moory <amaryasser2358@gmail.com>
2025-02-19 16:25:42 +0200

    create class Preson

---
+++ Hotel_project/Hotel_Classes/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Hotel_Classes
+{
+    internal abstract class Person
+    {
+        protected int Id;
+        protected string Name;
+        protected string Phone;
+
+        public Person(int id, string name, string phone)
+        {
+            Id = id;
+            Name = name;
+            Phone = phone;
+        }
+
+        public abstract void DisplayInfo();
+    }
+}
